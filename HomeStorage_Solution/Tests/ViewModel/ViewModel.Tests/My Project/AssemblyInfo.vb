1b2821efbe597b57d0c84a9931c635ebfaffd46e
NoFear23m <patschka.sascha@live.com>
2018-09-16 11:57:43 +0200

    erstellt erste Standard-Projektstruktur für eine korrekte MVVM Applikation

---
+++ HomeStorage_Solution/Tests/ViewModel/ViewModel.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("ViewModel.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ViewModel.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("52906879-6f2b-4ddc-beef-74e7488359b4")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
