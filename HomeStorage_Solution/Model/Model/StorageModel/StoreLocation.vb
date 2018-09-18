4c7068e101210226db5cbec3de6670bcbd81fcf7
MichaHo <43331983+MichaHo@users.noreply.github.com>
2018-09-18 11:05:54 +0200

    Klasse SubStore durch Klasse StoreLocation ersetzt Kommentar in Store.vb hinzugefügt

---
+++ HomeStorage_Solution/Model/Model/StorageModel/StoreLocation.vb
@@ -0,0 +1,20 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports Model.Interfaces
+
+Namespace StorageModel
+    Public Class StoreLocation
+        Inherits ModelBase
+        Implements ILogicalDelete
+
+        <Key>
+        Public Overridable Property StoreLocationId As Integer
+        <Required>
+        Public Overridable Property Title As String
+
+        Public Overridable Property StoreId As Integer
+        Public Overridable Property Store As Store
+        Public Property DeletedFlag As Boolean Implements ILogicalDelete.DeletedFlag
+        Public Property DeletedTimestamp As Date? Implements ILogicalDelete.DeletedTimestamp
+    End Class
+End Namespace
+
