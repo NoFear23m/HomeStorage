6967ab2a6af016f91dbc4cada713a34cd213182d
Patschka Sascha <patschka.sascha@live.com>
2018-09-25 18:25:47 +0200

    fügt Repositoryklassen hinzu

---
+++ HomeStorage_Solution/HomeStorage.DAL/ArticleRepository.vb
@@ -0,0 +1,18 @@
+﻿Imports HomeStorage.DbContext
+
+Public Class ArticleRepository
+    Inherits GenericRepository(Of Model.StorageModel.Article)
+
+    Public Sub New()
+        MyBase.New
+    End Sub
+
+    Public Sub New(context As Object)
+        MyBase.New(kontext:=CType(context, StorageContext))
+    End Sub
+
+    Friend Sub New(ctx As StorageContext)
+        MyBase.New(kontext:=ctx)
+    End Sub
+
+End Class
